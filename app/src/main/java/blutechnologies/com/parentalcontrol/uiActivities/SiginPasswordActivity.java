252fb1e990019fa42ac565d8208fb7551666050e
alyhassan551 <aly.hassan551@gmail.com>
2018-10-08 13:14:22 +0500

    kill

--- app/src/main/java/blutechnologies/com/parentalcontrol/uiActivities/SiginPasswordActivity.java
+++ app/src/main/java/blutechnologies/com/parentalcontrol/uiActivities/SiginPasswordActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 import blutechnologies.com.parentalcontrol.R;
 
@@ -15,9 +16,11 @@ public class SiginPasswordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sigin_password);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Toast.makeText(this, "ali hassan", Toast.LENGTH_SHORT).show();
     }
 
 }
