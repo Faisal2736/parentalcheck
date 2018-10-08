49ba3affd5ccb18eee6c76c8a446608eb602cc63
Faisal <faisal2736@gmail.com>
2018-10-08 14:36:15 +0500

    Splash complete

--- app/src/main/java/blutechnologies/com/parentalcontrol/uiActivities/SplashActivity.java
+++ app/src/main/java/blutechnologies/com/parentalcontrol/uiActivities/SplashActivity.java
@@ -1,8 +1,11 @@
 package blutechnologies.com.parentalcontrol.uiActivities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
 
 import blutechnologies.com.parentalcontrol.R;
 
@@ -12,6 +15,56 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        hideStatusBar();
+        splashThread();
 
+
+    }
+
+    public void hideStatusBar() {
+        View decorView = getWindow().getDecorView();
+
+
+// Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        decorView.setSystemUiVisibility(uiOptions);
+// Remember that you should never show the action bar if the
+// status bar is hidden, so hide that too if necessary.
+
+        // If the Android version is lower than Jellybean, use this call to hide
+        // the status bar.
+
+        if (Build.VERSION.SDK_INT < 16) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+
+    }
+
+    public void splashThread() {
+        Thread timerThread = new Thread() {
+            public void run() {
+                try {
+                    try {
+                        sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                } finally {
+
+                    Intent intent = new Intent(SplashActivity.this, WelcomeAcitivty.class);
+                    startActivity(intent);
+                    finish();
+
+                }
+
+            }
+        };
+        timerThread.start();
     }
 }
