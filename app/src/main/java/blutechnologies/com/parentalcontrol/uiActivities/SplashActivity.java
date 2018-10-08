04e296a83138d841dc5f8728ada3ad3eab17796d
Faisal <faisal2736@gmail.com>
2018-10-08 13:02:53 +0500

    initial commit

--- app/src/main/java/blutechnologies/com/parentalcontrol/uiActivities/SplashActivity.java
+++ app/src/main/java/blutechnologies/com/parentalcontrol/uiActivities/SplashActivity.java
@@ -2,6 +2,7 @@ package blutechnologies.com.parentalcontrol.uiActivities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import blutechnologies.com.parentalcontrol.R;
 
@@ -11,5 +12,6 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
     }
 }
