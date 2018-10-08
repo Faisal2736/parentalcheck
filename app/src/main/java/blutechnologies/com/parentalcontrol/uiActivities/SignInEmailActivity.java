468406cfb322bbee663e1ee7ea7dbffafef5228c
Faisal <faisal2736@gmail.com>
2018-10-08 14:45:55 +0500

    Splash complete

--- app/src/main/java/blutechnologies/com/parentalcontrol/uiActivities/SignInEmailActivity.java
+++ app/src/main/java/blutechnologies/com/parentalcontrol/uiActivities/SignInEmailActivity.java
@@ -1,18 +1,30 @@
 package blutechnologies.com.parentalcontrol.uiActivities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 import blutechnologies.com.parentalcontrol.R;
 
 public class SignInEmailActivity extends AppCompatActivity {
+    private Button btn_next_signIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_email);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        btn_next_signIn = findViewById(R.id.btn_next_signIn);
+
+        btn_next_signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignInEmailActivity.this, SiginPasswordActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
