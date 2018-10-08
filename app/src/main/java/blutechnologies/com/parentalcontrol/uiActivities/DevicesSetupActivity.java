517979ab554f8df73f1f7fafc56aa072ce7ab9ed
Faisal <faisal2736@gmail.com>
2018-10-08 15:13:26 +0500

    Child

--- app/src/main/java/blutechnologies/com/parentalcontrol/uiActivities/DevicesSetupActivity.java
+++ app/src/main/java/blutechnologies/com/parentalcontrol/uiActivities/DevicesSetupActivity.java
@@ -1,11 +1,7 @@
 package blutechnologies.com.parentalcontrol.uiActivities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import blutechnologies.com.parentalcontrol.R;
 
@@ -15,7 +11,6 @@ public class DevicesSetupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_devices_setup);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
