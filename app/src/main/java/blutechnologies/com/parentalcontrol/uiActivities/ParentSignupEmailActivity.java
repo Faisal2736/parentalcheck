df6ce96d1f99caf2c03fc5f00da58dcc0efc7d70
alyhassan551 <aly.hassan551@gmail.com>
2018-10-10 11:57:27 +0500

    Add

--- app/src/main/java/blutechnologies/com/parentalcontrol/uiActivities/ParentSignupEmailActivity.java
+++ app/src/main/java/blutechnologies/com/parentalcontrol/uiActivities/ParentSignupEmailActivity.java
@@ -3,17 +3,27 @@ package blutechnologies.com.parentalcontrol.uiActivities;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 
 import blutechnologies.com.parentalcontrol.R;
 
 public class ParentSignupEmailActivity extends AppCompatActivity {
 
     Button btnNext;
+
+    TextInputLayout firstNameTextInput;
+    TextInputLayout lastNameTextInput;
+    TextInputLayout emailTextInput;
+
+    EditText firstName;
+    EditText lastName;
+    EditText email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +38,14 @@ public class ParentSignupEmailActivity extends AppCompatActivity {
     private void initViews() {
 
         btnNext = findViewById(R.id.btnNext);
+
+        firstNameTextInput = findViewById(R.id.firstNameTextInput);
+        lastNameTextInput = findViewById(R.id.lastNameTextInput);
+        emailTextInput = findViewById(R.id.emailTextInput);
+
+        firstName = findViewById(R.id.firstName);
+        lastName = findViewById(R.id.lastName);
+        email = findViewById(R.id.email);
     }
 
     private void initListeners() {
@@ -36,7 +54,45 @@ public class ParentSignupEmailActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                if (email.getText().toString().isEmpty() || firstName.getText().toString().isEmpty() ||
+                        lastName.getText().toString().isEmpty()) {
+
+                    if (firstName.getText().toString().isEmpty()) {
+                        firstNameTextInput.setErrorEnabled(true);
+                        firstNameTextInput.setError("First name cannot be empty");
+                    } else {
+                        firstNameTextInput.setErrorEnabled(false);
+                    }
+
+                    if (lastName.getText().toString().isEmpty()) {
+                        lastNameTextInput.setErrorEnabled(true);
+                        lastNameTextInput.setError("Last name cannot be empty");
+                    } else {
+                        lastNameTextInput.setErrorEnabled(false);
+                    }
+
+                    if (email.getText().toString().isEmpty()) {
+                        emailTextInput.setErrorEnabled(true);
+                        emailTextInput.setError("Email name cannot be empty");
+                    } else {
+                        emailTextInput.setErrorEnabled(false);
+                    }
+
+                    return;
+                }
+
+                if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email.getText().toString()).matches()) {
+                    emailTextInput.setErrorEnabled(true);
+                    emailTextInput.setError("Please enter valid email address");
+                    return;
+                } else {
+                    emailTextInput.setErrorEnabled(false);
+                }
+
                 Intent intent = new Intent(ParentSignupEmailActivity.this, ParentSignupPasswordActivity.class);
+                intent.putExtra("firstName", firstName.getText().toString().isEmpty());
+                intent.putExtra("lastName", lastName.getText().toString().isEmpty());
+                intent.putExtra("email", email.getText().toString().isEmpty());
                 startActivity(intent);
             }
         });
--- app/src/main/java/blutechnologies/com/parentalcontrol/uiActivities/ParentSignupPasswordActivity.java
+++ app/src/main/java/blutechnologies/com/parentalcontrol/uiActivities/ParentSignupPasswordActivity.java
@@ -3,17 +3,25 @@ package blutechnologies.com.parentalcontrol.uiActivities;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 
 import blutechnologies.com.parentalcontrol.R;
 
 public class ParentSignupPasswordActivity extends AppCompatActivity {
 
     Button createAccount;
+
+    TextInputLayout passwordTextInput;
+    TextInputLayout confirmPasswordTextInput;
+
+    EditText password;
+    EditText confirmPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +36,12 @@ public class ParentSignupPasswordActivity extends AppCompatActivity {
     private void initViews() {
 
         createAccount = findViewById(R.id.createAccount);
+
+        passwordTextInput = findViewById(R.id.passwordTextInput);
+        confirmPasswordTextInput = findViewById(R.id.confirmPasswordTextInput);
+
+        password = findViewById(R.id.password);
+        confirmPassword = findViewById(R.id.confirmPassword);
     }
 
     private void initListeners() {
@@ -35,6 +49,30 @@ public class ParentSignupPasswordActivity extends AppCompatActivity {
         createAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                if (password.getText().toString().isEmpty()) {
+                    passwordTextInput.setErrorEnabled(true);
+                    passwordTextInput.setError("Password name cannot be empty");
+                    return;
+                } else {
+                    passwordTextInput.setErrorEnabled(false);
+                }
+
+                if (password.getText().toString().length() < 6) {
+                    passwordTextInput.setErrorEnabled(true);
+                    passwordTextInput.setError("Password must be atleast 6 characters");
+                    return;
+                } else {
+                    passwordTextInput.setErrorEnabled(false);
+                }
+
+                if (!password.getText().toString().equals(confirmPassword.getText().toString())) {
+                    confirmPasswordTextInput.setErrorEnabled(true);
+                    confirmPasswordTextInput.setError("Password mistatch");
+                    return;
+                } else {
+                    confirmPasswordTextInput.setErrorEnabled(false);
+                }
 
                 Intent intent = new Intent(ParentSignupPasswordActivity.this, ParentActivationCodeActivity.class);
                 startActivity(intent);
